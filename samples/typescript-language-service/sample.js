f518376955c24fa20ab2fa493267f23a36670b32
mkslanc <32402726+mkslanc@users.noreply.github.com>
2024-03-18 11:18:01 +0000

    Deploying to gh-pages from @ mkslanc/ace-playground@ee37edafedeb16f9155c13388b25f116867800bf 🚀

--- samples/typescript-language-service/sample.js
+++ samples/typescript-language-service/sample.js
@@ -1,9 +1,13 @@
 ace.require("ace/ext/language_tools");
-// Easiest way to run ACE editor in container with id="example"
 let editor = ace.edit("example", {
     theme: "ace/theme/textmate",
     mode: "ace/mode/typescript",
-    value: "console.log('Hello world' ;"
+    value: `import * as libFile from "./dir/file";
+
+let test;
+let chainable = new ChainableOne();
+chainable.chainableTwo.addAlpha("test").setBeta();
+libFile.data.setAlpha("").chainableTwo;`
 });
 // enable autocompletion and snippets
 editor.setOptions({
@@ -12,9 +16,37 @@ editor.setOptions({
     enableLiveAutocompletion: true
 });
 
-//create Language Provider for json doc from ace linters
-
+//create Language Provider for editor
 let provider = LanguageProvider.fromCdn("https://cdn.jsdelivr.net/npm/ace-linters/build");
 
 // Register the editor with the language provider
 provider.registerEditor(editor);
+
+provider.setGlobalOptions("typescript", {
+    errorCodesToTreatAsWarning: [
+        "6133"
+    ],
+    compilerOptions: {
+        noUnusedLocals: true
+    },
+    extraLibs: {
+        "libDeclaration.d.ts": {
+            content:
+`declare class ChainableOne {
+    chainableTwo: ChainableTwo;
+    setAlpha(value: string): this;
+    setBeta(value: number): ChainableTwo;
+}
+
+declare class ChainableTwo {
+    setGamma(value: boolean): this;
+    addAlpha(value: string): ChainableOne;
+}`,
+            version: 1
+        },
+        "dir/file.ts": {
+            content: "export var data = new ChainableOne();",
+            version: 1
+        }
+    }
+});
